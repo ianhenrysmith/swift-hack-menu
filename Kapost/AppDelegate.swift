2ef7c6985ac7b87a2d9a650a75a59d877fd3cfca
Ian Smith <ianhenrysmith@gmail.com>
2016-08-01 09:42:55 -0600

    Initial Commit

---
+++ Kapost/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Kapost
+//
+//  Created by Ian Smith on 8/1/16.
+//  Copyright © 2016 Kapost. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
